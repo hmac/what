3bca560802abf2697cbf331844297c4901f4b52d
Harry Maclean <harrymaclean@gocardless.com>
2017-05-29 12:58:20 +0100

    Add circleci config

--- spec/prepare.rb
+++ spec/prepare.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
-$LOAD_PATH << "lib"
-require_relative "spec_helper"
+require "what"
+require "what/migrations/v1"
+
+ActiveRecord::Base.establish_connection(adapter: "postgresql")
+What::Connection.connection = ActiveRecord::Base.connection
+
+ActiveRecord::Migration.run(What::Migrations::V1)
 
 What::Connection.execute("DROP TABLE IF EXISTS payments")
 What::Connection.execute("CREATE TABLE payments (amount integer)")
